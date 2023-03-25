fe572b07ca10f990676a0addedea3aaec449d7e7
Vworks <dpenlan1@kent.edu>
2023-03-24 20:22:10 -0400

    Menu

--- src/components/CustWindow/ChipList.tsx
+++ src/components/CustWindow/ChipList.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export const ChipList = () => {
+  const [chipList, setChipList] = useState([{}]);
+
+  useEffect(() => {
+    setChipList([{}, {}, {}, {}, {}, {}, {}, {}]);
+  }, []);
+
   return (
-    <div className="chip-list-container">ChipList</div>
-  )
-}
+    <div className="chip-list-container">
+      {chipList.map((chip, index) => (
+        <div className="list-chip" key={index}>
+          <div className="list-chip-icon"></div>
+          <div className="list-chip-letter" >X</div>
+        </div>
+      ))}
+    </div>
+  );
+};
--- src/components/CustWindow/ChipSelection.tsx
+++ src/components/CustWindow/ChipSelection.tsx
@@ -4,6 +4,8 @@ export const ChipSelection = () => {
   return (
     <div className="chip-selection-container">
         <div className="chip-selection">
+          <div className="navi-icon-container"></div>
+          <div className="chip-slot-container"></div>
         </div>    
     </div>
   )
