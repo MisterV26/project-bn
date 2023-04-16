75d0219ac5a5f8e997369ccbd4ab258b87ab1722
Vworks <dpenlan1@kent.edu>
2023-04-16 17:30:47 -0400

    Chip select container

--- src/components/CustWindow/ChipSelection.tsx
+++ src/components/CustWindow/ChipSelection.tsx
@@ -4,8 +4,16 @@ export const ChipSelection = () => {
   return (
     <div className="chip-selection-container">
         <div className="chip-selection">
-          <div className="navi-icon-container"></div>
-          <div className="chip-slot-container"></div>
+          <div className="navi-icon-container">
+            <div className="navi-icon"></div>
+          </div>
+          <div className="selected-chip-container">
+            <div className="selected-chip-slot"></div>
+            <div className="selected-chip-slot"></div>
+            <div className="selected-chip-slot"></div>
+            <div className="selected-chip-slot"></div>
+            <div className="selected-chip-slot"></div>
+          </div>
         </div>    
     </div>
   )
